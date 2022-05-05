f169ba3e546601b49ab0a62ebddba55c66730ffe
nadine-moser <69237268+nadine-moser@users.noreply.github.com>
2022-05-05 16:04:22 +0200

    add storage

--- App.tsx
+++ App.tsx
@@ -1,10 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const COLOR_STORAGE_KEY = '@color_Items';
+
+type Color = {
+  name: string,
+  key: number
+}
 
 export default function App() {
+  const [newColor, setNewColor] = useState("");
+  const [colors, setColors] = useState<Color[]>([]);
+
+  useEffect(() => {
+    loadColors();
+  }, []);
+
+  useEffect(() => {
+    updateColors();
+    console.log(colors);
+  }, [colors]);
+
+  const getMaxKey = () : number => {
+    let maxNumber: number = 0;
+    colors.forEach(x => x.key > maxNumber ? maxNumber = x.key : null);
+    return maxNumber
+  }
+
+  const addColor = () : void => {
+    if (newColor != null && newColor != "") {
+      setColors([...colors, { name: newColor, key: getMaxKey()+1 }]);
+    }
+  }
+  const deleteColors = () : void => {
+    setColors([]);
+  }
+
+  const loadColors = async () => {
+    try {
+      let storedColors = await AsyncStorage.getItem(COLOR_STORAGE_KEY);
+      if (storedColors != null) {
+        setColors(JSON.parse(storedColors));
+      }
+    } catch (e) {
+      console.error("failed to load colors", e);
+    }
+  }
+
+  const updateColors = async () => {
+    try {
+      let todoItemsToStore = JSON.stringify(colors);
+      await AsyncStorage.setItem(COLOR_STORAGE_KEY, todoItemsToStore);
+    } catch (e) {
+      console.error("failed to store colors", e);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>new Color: {newColor}{'\n'}</Text>
+      
+      <Text style={styles.title}>all Colors:</Text>
+      {colors.map(x => <Text>{x.name}</Text>)}
+      <Text>{'\n'}</Text>
+      <TextInput placeholder='please enter a text' onChangeText={(x) => setNewColor(x)} />
+      <Button title="add color" onPress={addColor} />
+      <Button title="delete all" onPress={deleteColors} />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +80,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontWeight : 'bold'
+  }
 });
